dea0360ff94f3791c21662b035591ca3a67b4ebf
o-Arcanjo <oarcanjovalentim@gmail.com>
2025-05-02 15:43:51 -0300

    refactor:adicionar camada de utilizações genéricas

--- src/main/java/com/filmchoice/util/GetPropertiesBd.java
+++ src/main/java/com/filmchoice/util/GetPropertiesBd.java
@@ -1,13 +1,15 @@
 package com.filmchoice.util;
 import java.util.Properties;
+import com.filmchoice.config.TipoConexao;
 
 public final class GetPropertiesBd{
     private GetPropertiesBd(){};
 
-    public static ConfigVariavel getInstanceConfigVariavel(Properties props){
-            String dbUrl = props.getProperty("DB_URL");
-            String dbUser = props.getProperty("DB_USER");
-            String dbSenha = props.getProperty("DB_PASSWORD");
+    public static ConfigVariavel getInstanceConfigVariavel(Properties props, TipoConexao conexao){
+            String prefixo = conexao.toString().toLowerCase() + ".";
+            String dbUrl = props.getProperty(prefixo + "DB_URL");
+            String dbUser = props.getProperty(prefixo + "DB_USER");
+            String dbSenha = props.getProperty(prefixo + "DB_PASSWORD");
             return new ConfigVariavel
                 .Builder()
                 .url(dbUrl)
