2f5e949ba06cd94c9b64fcfe46a3caf43c8ee0d8
Thiago1722 <thiagosbthiago@gmail.com>
2025-06-03 16:40:55 -0300

    add

--- src/main/java/com/filmchoice/entities/Genero.java
+++ src/main/java/com/filmchoice/entities/Genero.java
@@ -1,7 +1,17 @@
 package com.filmchoice.entities;
-import jakarta.persistence.*;
-
+import java.util.List;
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Genero")
