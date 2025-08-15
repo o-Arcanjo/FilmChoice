5a0f77628ce02803d28e9245c5fc1310aa5a8809
o-Arcanjo <oarcanjovalentim@gmail.com>
2025-08-14 22:51:01 -0300

    feat:adicionar spring boot

---
+++ src/main/java/com/filmchoice/FilmChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.filmchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmChoiceApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(FilmChoiceApplication.class, args);
+    }
+    
+}
--- src/main/java/com/filmchoice/dao/impl/UsuarioDAOImpl.java
+++ src/main/java/com/filmchoice/dao/impl/UsuarioDAOImpl.java
@@ -3,20 +3,21 @@ package com.filmchoice.dao.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import com.filmchoice.dao.PersistenciaDawException;
+import com.filmchoice.dao.UsuarioDAO;
 import com.filmchoice.entities.Usuario;
 
-import jakarta.persistence.EntityManagerFactory;
-
-import com.filmchoice.dao.UsuarioDAO;
-
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.NoResultException;
 
+@Repository  // <- Spring consegue injetar
 public class UsuarioDAOImpl extends AbstractDAOImpl<Usuario, Long> implements UsuarioDAO {
 
-    public UsuarioDAOImpl(Class<Usuario> entityClass, EntityManagerFactory emf) {
-        super(entityClass, emf);
+    public UsuarioDAOImpl(EntityManagerFactory emf) {
+        super(Usuario.class, emf);
     }
 
     @Override
@@ -35,49 +36,28 @@ public class UsuarioDAOImpl extends AbstractDAOImpl<Usuario, Long> implements Us
         }
     }
 
-
     @Override
     public void save(Usuario usuario) throws PersistenciaDawException {
-        try {
-            super.save(usuario);
-        } catch (PersistenciaDawException e) {
-            throw new PersistenciaDawException("Erro ao salvar Usuário", e);
-        }
+        super.save(usuario);
     }
 
     @Override
     public Usuario update(Usuario usuario) throws PersistenciaDawException {
-        try {
-            return super.update(usuario);
-        } catch (PersistenciaDawException e) {
-            throw new PersistenciaDawException("Erro ao atualizar Usuário", e);
-        }
+        return super.update(usuario);
     }
 
     @Override
     public Usuario getByID(Long id) throws PersistenciaDawException {
-        try {
-            return super.getByID(id);
-        } catch (PersistenciaDawException e) {
-            throw new PersistenciaDawException("Erro ao buscar Usuário por ID", e);
-        }
+        return super.getByID(id);
     }
 
     @Override
     public List<Usuario> getAll() throws PersistenciaDawException {
-        try {
-            return super.getAll();
-        } catch (PersistenciaDawException e) {
-            throw new PersistenciaDawException("Erro ao listar todos os Usuários", e);
-        }
+        return super.getAll();
     }
 
     @Override
     public void delete(Long id) throws PersistenciaDawException {
-        try {
-            super.delete(id);
-        } catch (PersistenciaDawException e) {
-            throw new PersistenciaDawException("Erro ao deletar Usuário", e);
-        }
+        super.delete(id);
     }
 }
